712c8a0674c168a29e2860033013311487184313
Zhong Xin <zhongxin2506@outlook.com>
2020-02-11 08:50:11 +0800

    练习2.4

---
+++ 2.4_cdr.rkt
@@ -0,0 +1,14 @@
+#lang racket
+
+(define (cons x y)
+  (lambda (m) (m x y)))
+
+(define (car z)
+  (z (lambda (p q) p)))
+
+(define (cdr z)
+  (z (lambda (p q) q)))
+
+(define a (cons 1 2))
+(car a)
+(cdr a)
